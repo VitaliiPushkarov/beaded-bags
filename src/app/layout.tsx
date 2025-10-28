72c4d9a2bcf2d1803e87cb7eab13032f5fee7eec
vitaliipushkarov <vitalypushkarov@gmail.com>
2025-10-28 18:27:08 +0200

    updated favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import Header from '../components/Header'
 import CartDrawer from '@/components/cart/CartDrawer'
+import Footer from '@/components/Footer'
 
 export const metadata: Metadata = { title: 'GERDAN' }
 
@@ -12,10 +13,32 @@ export default function RootLayout({
 }) {
   return (
     <html lang="uk">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link
+          rel="icon0"
+          href="/icon.svg"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+        <link
+          rel="icon1"
+          href="/icon.png"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+        <link
+          rel="apple-touch-icon"
+          href="/apple-icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+      </head>
       <body className="min-h-screen text-gray-900 antialiased bg-white font-fixel">
         <Header />
         <main className="max-w-full mx-auto md:py-8">{children}</main>
         <CartDrawer />
+        <Footer />
       </body>
     </html>
   )
