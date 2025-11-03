64c278ac18f38919444ac7ae882eb3b988500e6d
vitaliipushkarov <vitalypushkarov@gmail.com>
2025-11-03 21:58:37 +0200

    updated sitemap configuration and regenerated site map files to reflect recent changes in website structure.

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -5,15 +5,28 @@ module.exports = {
   sitemapSize: 7000,
   changefreq: 'weekly',
   priority: 0.8,
-  exclude: ['/api/*', '/checkout/*'],
+
+  exclude: [
+    '/api/*',
+    '/checkout*',
+    '/cart',
+    '/cashback',
+    '/manifest.json',
+    '/icon*',
+    '/_next/*',
+  ],
+
   robotsTxtOptions: {
     policies: [
       {
         userAgent: '*',
         allow: '/',
-        disallow: ['/api/', '/checkout/'],
+        disallow: ['/api/', '/checkout', '/cart', '/cashback'],
       },
     ],
-    additionalSitemaps: ['https://gerdan.online/sitemap.xml'],
+    additionalSitemaps: [
+      'https://gerdan.online/sitemap.xml',
+      'https://gerdan.online/sitemap-0.xml',
+    ],
   },
 }
