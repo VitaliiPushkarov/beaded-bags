7df1703ecede653e69b9c64ca3fe2dd9bcd5d39c
vitaliipushkarov <vitalypushkarov@gmail.com>
2025-11-10 21:12:31 +0200

    removed i18n due to i18n configuration in next.config.ts is unsupported in App Router.

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,7 @@
 import type { NextConfig } from 'next'
 
-const I18NConfig = {
-  i18n: {
-    locales: ['uk', 'en'],
-    defaultLocale: 'uk',
-
-    domains: [
-      {
-        domain: 'gerdan.online',
-        defaultLocale: 'uk',
-      },
-      {
-        domain: 'ca.gerdan.online',
-        defaultLocale: 'en',
-      },
-    ],
-  },
-}
 const nextConfig: NextConfig = {
   transpilePackages: ['swiper'],
-  ...I18NConfig,
 }
 
 export default nextConfig
