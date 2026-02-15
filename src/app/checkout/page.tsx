342534cb63f63dc67aa393f9e3f1ca9656bff997
Vitalii Pushkarov <vitalypushkarov@gmail.com>
2026-02-15 15:46:13 +0200

    minor update

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from 'react'
 import CheckoutClient from './CheckoutClient'
 
 export default function CheckoutPage() {
-  return <CheckoutClient />
+  return (
+    <Suspense fallback={<div className="p-6 text-center">Завантаження…</div>}>
+      <CheckoutClient />
+    </Suspense>
+  )
 }
